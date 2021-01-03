4e9e975ddbf67a78bd0d6a6d10f99d2a480183a6
AlexAegis <alexaegis@gmail.com>
2021-01-03 11:10:28 +0100

    chore: moved e file

--- e.user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# pont itself reads this file before doing anything with the module
-# to load necessary environment variables.
-
-# shellcheck disable=SC1091
-. "./XDG_CONFIG_HOME.pont/environment.d/90-pont.conf"
